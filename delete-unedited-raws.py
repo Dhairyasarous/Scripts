ab986e043ab757a011f4dbfd90cb3518f9dfa306
dhairyasarous <dp-1994@hotmail.com>
2017-06-28 14:11:15 -0700

    updated for clarity

--- delete-unedited-raws.py
+++ delete-unedited-raws.py
@@ -7,6 +7,7 @@ def main():
     ext_raw = ".CR2"
     ext_metadata = ".xmp"
 
+    # Argument Parsing
     parser = argparse.ArgumentParser(description='Delete raw files which do not have corresponding metadata file ('
                                                  'e.g. .xmp). Run AFTER editing pictures and only if the editing '
                                                  'software creates a metadata file in the same location.')
@@ -16,7 +17,6 @@ def main():
     args = parser.parse_args()
 
     dir_location = args.location
-    os.chdir(dir_location)
     if not (args.extension1 is None):
         ext_raw = args.extension1
     if not (args.extension2 is None):
@@ -26,23 +26,24 @@ def main():
     print("Raw Extension:", ext_raw)
     print("Metadata Extension:", ext_metadata, "\n")
 
-    edited_files = set()
+    os.chdir(dir_location)
+
+    # create a collection of edited raw files
+    edited_raws = set()
 
     for file in os.listdir(dir_location):
         if file.endswith(ext_metadata):
-            file_path = os.path.basename(file)
-            file_name = os.path.splitext(file_path)[0]
-            edited_files.add(file_name)
+            file_name = os.path.splitext(os.path.basename(file))[0] + ext_raw
+            edited_raws.add(file_name)
 
     print("The following files are moved to trash/recycling bin:")
 
+    # delete files not in the edited_raw collection
     for file in os.listdir(dir_location):
         if file.endswith(ext_raw):
-            file_name = os.path.splitext(os.path.basename(file))[0]
-            unedited_file_path = file_name + ext_raw
-            if file_name not in edited_files:
-                send2trash(unedited_file_path)
-                print(unedited_file_path)
+            if file not in edited_raws:
+                send2trash(file)
+                print(file)
 
     print("\nDone!")
 
